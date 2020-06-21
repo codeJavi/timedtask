7cd061e1cf229a37d38eb41c4ddf4bd34c9d6b86
Javi Leyva <jvrleyva@gmail.com>
2020-06-21 03:24:05 +0000

    modified bin folder

--- lib/timedtask.rb
+++ lib/timedtask.rb
@@ -1,3 +1,16 @@
-require_relative './lib/bundler/setup'
-require_relative './lib/timedtask/version'
-require_relative './lib/timedtask/cli' 
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+
+
+require_relative './timedtask/api'
+require_relative './timedtask/version'
+require_relative './timedtask/cli'
+
+
+
+ 
+
+module Timedtask
+end 
